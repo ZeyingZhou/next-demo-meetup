4f6de9a5b1a79a5a94861b839293975c9e6ba1c4
ZeyingZhou <47362333+ZeyingZhou@users.noreply.github.com>
2022-11-03 01:15:02 -0400

    fixed

--- pages/index.js
+++ pages/index.js
@@ -20,21 +20,7 @@ function HomePage(props) {
 
 
 
-// export async function getServerSideProps(context) {
-//     const req = context.req;
-//     const res = context.res;
-
-//     // fetch data from an API
-
-//     return {
-//         props: {
-//             meetups: DUMMY_MEETUPS
-//         }
-//     };
-// }
-
-export async function getStaticProps() {
-    // fetch data from an API
+export async function getServerSideProps() {
     const client = await MongoClient.connect('mongodb+srv://Zeying:KcXMvp51ZQz3aUtL@cluster0.ipkzbxh.mongodb.net/meetups?retryWrites=true&w=majority');
     const db = client.db();
 
@@ -55,5 +41,28 @@ export async function getStaticProps() {
         revalidate: 1
     };
 }
+
+// export async function getStaticProps() {
+//     // fetch data from an API
+//     const client = await MongoClient.connect('mongodb+srv://Zeying:KcXMvp51ZQz3aUtL@cluster0.ipkzbxh.mongodb.net/meetups?retryWrites=true&w=majority');
+//     const db = client.db();
+
+//     const meetupsCollection = db.collection('meetups');
+
+//     const meetups = await meetupsCollection.find().toArray();
+//     client.close();
+
+//     return {
+//         props: {
+//             meetups: meetups.map(meetup => ({
+//                 title: meetup.title,
+//                 address: meetup.address,
+//                 image: meetup.image,
+//                 id: meetup._id.toString()
+//             }))
+//         },
+//         revalidate: 1
+//     };
+// }
 
 export default HomePage;
